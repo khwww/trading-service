5be1a59da59968386a42d1bba88c88fcb85aab9f
khwww <khw4420@knu.ac.kr>
2025-11-14 18:53:43 +0900

    feat: 모든 페이지에 공통 헤더 적용

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
-import Header from '@/components/Header';
 import MarketOverview from '@/components/MarketOverview';
 import StockRanking from '@/components/StockRanking';
 
 export default function Home() {
   return (
     <div className="min-h-screen">
-      <Header />
       <main className="flex-1 mr-0 p-6 pr-24">
         <MarketOverview />
         <StockRanking />
