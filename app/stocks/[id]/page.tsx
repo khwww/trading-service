9f5ba6e8aa7c1e28fba50c7b08dbd3188c0dc174
khwww <144529720+khwww@users.noreply.github.com>
2025-11-21 00:31:24 +0900

    design: 상세페이지 패딩 설정

--- app/stocks/[id]/page.tsx
+++ app/stocks/[id]/page.tsx
@@ -8,45 +8,48 @@ import TradingPanel from '@/components/detail/TradingPanel';
 export default function StockDetailPage() {
   return (
     <div className="min-h-screen bg-black">
-      {/* 헤더 */}
-      <StockHeader />
+      {/* 전체 컨텐츠 영역 */}
+      <div className="pl-6 detail-content-padding">
+        {/* 헤더 */}
+        <StockHeader />
 
-      {/* 탭 메뉴 */}
-      <div className="bg-black border-b border-gray-800">
-        <div className="flex items-center gap-6 px-4 detail-content-padding">
-          <button className="py-3 border-b-2 border-white text-white font-medium">
-            차트 · 호가
-          </button>
-          <button className="py-3 text-gray-400 hover:text-white">
-            종목정보
-          </button>
-          <button className="py-3 text-gray-400 hover:text-white">
-            뉴스 · 공시
-          </button>
-          <button className="py-3 text-gray-400 hover:text-white">
-            거래현황
-          </button>
-          <button className="py-3 text-gray-400 hover:text-white">
-            커뮤니티
-          </button>
-        </div>
-      </div>
-
-      {/* 메인 컨텐츠 */}
-      <div className="grid grid-cols-1 lg:grid-cols-12 gap-0 detail-content-padding">
-        {/* 차트 영역 */}
-        <div className="lg:col-span-6 border-r border-gray-800">
-          <StockChart />
+        {/* 탭 메뉴 */}
+        <div className="bg-black border-b border-gray-800">
+          <div className="flex items-center gap-6">
+            <button className="py-3 border-b-2 border-white text-white font-medium">
+              차트 · 호가
+            </button>
+            <button className="py-3 text-gray-400 hover:text-white">
+              종목정보
+            </button>
+            <button className="py-3 text-gray-400 hover:text-white">
+              뉴스 · 공시
+            </button>
+            <button className="py-3 text-gray-400 hover:text-white">
+              거래현황
+            </button>
+            <button className="py-3 text-gray-400 hover:text-white">
+              커뮤니티
+            </button>
+          </div>
         </div>
 
-        {/* 호가창 영역 */}
-        <div className="lg:col-span-3 border-r border-gray-800">
-          <OrderBook />
-        </div>
+        {/* 메인 컨텐츠 */}
+        <div className="grid grid-cols-1 lg:grid-cols-12 gap-0">
+          {/* 차트 영역 */}
+          <div className="lg:col-span-6 border-r border-gray-800">
+            <StockChart />
+          </div>
 
-        {/* 주문하기 패널 */}
-        <div className="lg:col-span-3">
-          <TradingPanel />
+          {/* 호가창 영역 */}
+          <div className="lg:col-span-3 border-r border-gray-800">
+            <OrderBook />
+          </div>
+
+          {/* 주문하기 패널 */}
+          <div className="lg:col-span-3">
+            <TradingPanel />
+          </div>
         </div>
       </div>
     </div>
